fdcc033418ab5423759001a234f90633df165905
pranith reddy saireddy <psaireddy@usf.edu>
2022-03-13 12:58:35 -0400

    Initial push

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// const searchingBook = () =>{
+//     let btn = document.getElementById('cta-btn');
+//     let overlay = document.getElementById('overlay');
+
+//     btn.addEventListener('click', () => {
+//         overlay.style.display = 'grid';
+//         overlay.classList.add('animate-overlay')
+//     })
+
+//     delay(3000).then(() => {
+//         let overlay1 = document.getElementById('overlay');
+//         overlay1.style.display = "none";
+//     })
+    
+// }
+
+function navSlide() {
+    let burger = document.getElementById('burger');
+    let nav = document.getElementById('nav-links');
+    let navLinks = document.querySelectorAll('.nav-links li');
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+        console.log(index)
+        if(link.style.animation) {
+            link.style.animation = '';
+        }
+        else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.4}s`;
+        }
+    });
+
+    burger.classList.toggle('toggle');
+}
+
+
+// const app = () => {
+//     navSlide();
+//     // searchingBook();
+    
+// }
+
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+
+function test(){
+    alert("Something")
+}
